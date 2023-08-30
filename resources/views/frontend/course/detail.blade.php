b623bd0ec142eded1622c72b7d6f72b69e485e45
HANWINMAUNG <hwn372013@gmail.com>
2023-08-30 08:26:45 -0700

    Ui

--- resources/views/frontend/course/detail.blade.php
+++ resources/views/frontend/course/detail.blade.php
@@ -2,12 +2,12 @@
 @section('content')
 <main>
       <section class="slider-area slider-area2">
-               <div class="slider-active">
+               <div class="slider-active" style="height:300px;">
                      <div class="single-slider slider-height2">
                            <div class="container">
                               <div class="row">
                                     <div class="col-xl-8 col-lg-11 col-md-12">
-                                          <div class="hero__caption hero__caption2">
+                                          <div class="hero__caption hero__caption2" style="padding-top:130px;">
                                              <h1 data-animation="bounceIn" data-delay="0.2s">Course Detail</h1>
                                              <nav aria-label="breadcrumb">
                                                 <ol class="breadcrumb">
